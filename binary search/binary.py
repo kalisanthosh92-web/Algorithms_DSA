e48667ed1859d2816c6c5394c20c66b2103682d5
kalisanthosh92 <kalisanthosh92@gmail.com>
2026-02-28 20:44:13 +0800

    ...

---
+++ binary search/binary.py
@@ -0,0 +1,19 @@
+def binary_search(list,item):
+    low = 0
+    high = len(list) -1 
+
+    while low <= high:
+        mid = (low + high)
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid -1
+        else:
+            low = mid +1
+
+my_list = [1,2,3,6,8]
+print(binary_search(my_list,6))
+print(binary_search(my_list,-4))
+
+
