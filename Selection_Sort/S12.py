bb43e5b7eba251022971b925327ce002b9818819
kalisanthosh92 <kalisanthosh92@gmail.com>
2026-03-07 04:11:39 +0800

    .

--- Selection_Sort/S12.py
+++ Selection_Sort/S12.py
@@ -1,14 +1,24 @@
 #Write selection sort that sorts only even numbers and leaves odd numbers in place.
 
 a = [7, 4, 3, 8, 5, 2]
-min_j = 0
+
+evens = []
+for x in a:
+    if x%2 == 0:
+        evens.append(x)
+
+
+for i in range(len(evens)):
+    min_i = i 
+    for j in range(i+1,len(evens)):
+        if evens[j] < evens[min_i]:
+            min_i = j
+    evens[i],evens[min_i] = evens[min_i],evens[i]
+
+k = 0 
 for i in range(len(a)):
-    
-    for j in range(i+1,len(a)):
-        if a[j]//2 == 0 and a[i]//2 == 0:
-            if a[j] > a[i]:
-                a[i],a[j] = a[j],a[i]
-    
-                    
-        
-print(a)   
+    if a[i]%2 == 0:
+        a[i] = evens[k]
+        k += 1
+print(a)
+
