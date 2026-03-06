40c0ed58452e72581344eaf6cc21cbf956d2740c
kalisanthosh92 <kalisanthosh92@gmail.com>
2026-03-06 16:56:59 +0800

    ...

---
+++ Selection.py
@@ -0,0 +1,18 @@
+def findsmllest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1,len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+    
+def selectionSort(arr):
+    newarr = []
+    for i in range(len(arr)):
+        smallest = findsmllest(arr)
+        newarr.append(arr.pop(smallest))
+    return newarr
+
+print(selectionSort([5,3,6,2,10]))
+
